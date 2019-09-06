da599d549e3ca5a57b8cd4733557e5bab9207808
rjo <onyettr@gmail.com>
2019-09-06 00:09:25 -0700

    added tokenizer functions

--- tokenizer.c
+++ tokenizer.c
@@ -13,6 +13,7 @@ Includes
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include <time.h>
 #include "basic.h"
@@ -56,19 +57,117 @@ Prototypes of all functions contained in this file (in order of occurance)
 ******************************************************************************
 */
 
+/**
+ * @brief     Process Special token
+ * @fn        Token_t TokenGetNumber(char *Bufferp, char *Tokenp) 
+ * @param[in] Bufferp - Buffer to tokenize
+ * @param[out]Tokenp  - add to tokenized buffer
+ * @return    Token_t 
+ * @notes     
+ */
+Token_t TokenGetNumber(char *Bufferp, char *Tokenp) {
+  printf("TokenGetNUmber\n");
+
+  return TOKEN_ERROR;
+}
+
+/**
+ * @brief     Process word (alpha) token
+ * @fn        Token_t TokenGetWord  (char *Bufferp, char *Tokenp) 
+ * @param[in] Bufferp - Buffer to tokenize
+ * @param[out]Tokenp  - add to tokenized buffer
+ * @return    Token_t 
+ * @notes     
+ */
+Token_t TokenGetWord  (char *Bufferp, char *Tokenp) {
+  printf("TokenGetWord %c\n", *Bufferp);
+  
+  while ( ((isalnum(*Bufferp)) || (*Bufferp != '\0')) && (isspace(*Bufferp)) ) {
+      *Tokenp++ = *Bufferp++;
+      printf("%c ", *Bufferp);
+      if (isalnum(*Bufferp)) {
+        printf("alpha ");
+      } else if (isspace(*Bufferp)) {
+        printf("space ");
+      } 
+  }
+
+  *Tokenp = '\0';
+  
+  return TOKEN_WORD;
+}
+
+/**
+ * @brief     Process Special token
+ * @fn        Token_t TokenGetSpecial(char *Bufferp, char *Tokenp) 
+ * @param[in] Bufferp - Buffer to tokenize
+ * @param[out]Tokenp  - add to tokenized buffer
+ * @return    Token_t 
+ * @notes     
+ */
+Token_t TokenGetSpecial(char *Bufferp, char *Tokenp) {
+    printf("TokenGetSpecial\n");
+
+    return TOKEN_ERROR;    
+}
+
+/**
+ * @brief     Token types to string
+ * @fn        char *TokenGetStringType(Token_t Token)
+ * @param[in] Token
+ * @return    char*
+ * @notes     
+ */
+char *TokenGetStringType(Token_t Token) {
+
+  switch (Token) {
+     case TOKEN_WORD:   return("WORD"); break;
+     case TOKEN_DIGIT:  return("DIGIT"); break;
+     case TOKEN_LETTER: return ("LETTER"); break;
+     case TOKEN_ERROR:  return ("ERROR"); break;
+     default: return ("????"); break;
+  }
+}
+
+/**
+ * @brief     Tokenize a given file 
+ * @fn        int32_t Tokenize (char *FileName)
+ * @param[in] FileName - file containing lines to Tokenize
+ * @return    int32_t ErrorCode
+ * @notes     
+ */
 int32_t Tokenize (char *FileName) {
   FILE *fp;
   int32_t ErrorCode = SUCCESS;
-  char *BufferP;
+  char *Bufferp;
+  char *Tokenp;
+  Token_t Token;  
 
   fp = fopen(FileName, "r");
     
-  BufferP = SourceBuffer;
-  while (UtilsReadSourceLine(fp, BufferP) == true) {
-     BufferP = UtilsSkipSpaces(BufferP);
+  Bufferp = SourceBuffer;
+  Tokenp  = TokenBuffer;
+  
+  while (UtilsReadSourceLine(fp, Bufferp) == true) {
+     Bufferp = UtilsSkipSpaces(Bufferp);
+
+     if (isdigit(*Bufferp)) {
+       Token = TokenGetNumber(Bufferp, Tokenp);
+     } else if (isalnum(*Bufferp)) {
+       Token = TokenGetWord(Bufferp, Tokenp);
+       printf("WORD %s %s\n", Bufferp, Tokenp);
+     } else {
+       Token = TokenGetSpecial(Bufferp, Tokenp);       
+     }
+
+     printf(">> %s %s\n", TokenGetStringType(Token), Tokenp);
+
   }
 
+
+
   UNUSED(TokenBuffer);
+  UNUSED(Token);
   
   return ErrorCode;
 }
