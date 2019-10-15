1ccb7206cc548ee831345a9f269d4583bcbf70f7
rjo <onyettr@gmail.com>
2019-10-15 06:16:37 +0100

    missed a file

---
+++ include/symboltable.h
@@ -0,0 +1,96 @@
+/**
+ *****************************************************************************
+ * @brief    Basic interpreter
+ * @author   onyettr
+ * @file     symboltable.h
+ * @defgroup symboltable
+ *           Utilities to drive the symbol table
+ ***************************************************************************** 
+ */
+#ifndef __SYMBOLTABLE__H__
+#define __SYMBOLTABLE__H__
+
+/*
+******************************************************************************
+Includes
+******************************************************************************
+*/
+
+/*
+******************************************************************************
+Private Types
+*****************************************************************************
+*/
+/**
+ *  @brief Value_t enumeration for vakues
+ *  @addtogrpup Utilities
+ */
+typedef union {
+  int32_t IntegerValue;        /*!> Integer value        */
+  float   RealValue;           /*!> Floating point value */
+  uint8_t CharacterValue;      /*!> Character value      */
+  uint8_t *String;             /*!> String valie         */
+} Value_t;
+
+typedef enum {
+  KEY_UNDEFINED,               /*!> Variable is undefined */
+  KEY_CONSTANT,                /*!> variable is constant  */
+  KEY_VARIABLE                 /*!> variable is variable  */
+} DefinitionKey_t;
+
+typedef struct {
+  DefinitionKey_t Key;
+  union {
+    struct {
+      Value_t value;
+    } Constant;
+
+    struct {
+      int32_t ParameterCount;
+      int32_t TotalLocalSize;
+      int32_t TotalParameterSize;
+    } Routine;
+
+    
+  } Information;
+} DefinitionStruct_t;
+
+typedef struct SymbolTableNode {
+  struct SymbolTableNode_t *pLeft;
+  struct SymbolTableNode_t *pRight;  
+  struct SymbolTableNode_t *pNext;
+  char *NameString;
+  char *InfoString;
+  DefinitionStruct_t      Defun;
+  int32_t NestingLevel;
+  int32_t LabelIndex;
+} SymbolTableNode_t, *SymbolTableNodePtr_t;
+
+/*
+******************************************************************************
+Private variables (static)
+******************************************************************************
+*/
+
+/*
+******************************************************************************
+Global variables
+******************************************************************************
+*/
+
+/*
+******************************************************************************
+Exported Global variables
+******************************************************************************
+*/
+
+/*
+******************************************************************************
+Prototypes of all functions contained in this file (in order of occurance)
+******************************************************************************
+*/
+SymbolTableNodePtr_t SearchSymbolTable (char *Name, SymbolTableNode_t *pNode);
+SymbolTableNodePtr_t AddSymbolTable    (char *Name, SymbolTableNode_t **pNode);
+SymbolTableNodePtr_t ShowSymbolTable   (SymbolTableNode_t *pNode) {
+  
+#endif  /* __SYMBOLTABLE__H__ */
